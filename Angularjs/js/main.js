d1696654c6e3ad26924546d53c2a5e2739fd9089
xiechangqing <576535019@qq.com>
2016-10-13 17:34:50 +0800

    AngularJs

---
+++ Angularjs/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by amyqing719@gmail.com on 2016/10/13.
+ */
+var app = angular.module("myApp",[]);
+app.controller('myCtrl',function($scope){
+    $scope.firstName = "Amy";
+    $scope.lastName = "Shieh";
+    });
